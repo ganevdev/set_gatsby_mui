99daee8bf5527d2d9f5357587a6d21abb8f3175a
Ivan Ganev <iganevru@gmail.com>
2018-09-02 02:17:05 +0300

    ini

--- src/components/RadioFilter.js
+++ src/components/RadioFilter.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  formControl: {
-    margin: theme.spacing.unit * 3,
-  },
-  group: {
-    margin: `${theme.spacing.unit}px 0`,
-  },
-});
 
 class RadioButtonsGroup extends React.Component {
   state = {
@@ -30,16 +15,12 @@ class RadioButtonsGroup extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
-
     return (
-      <div className={classes.root}>
-        <FormControl component="fieldset" className={classes.formControl}>
-          <FormLabel component="legend">Sensor</FormLabel>
+      <div>
+        <FormControl component="fieldset">
           <RadioGroup
             aria-label="sensor"
             name="sensor"
-            className={classes.group}
             value={this.state.value}
             onChange={this.handleChange}
           >
@@ -57,4 +38,4 @@ RadioButtonsGroup.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(RadioButtonsGroup);
+export default RadioButtonsGroup;
